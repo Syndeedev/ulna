28b85a726c0524cf890f9b91a7c908ab78c8ecc1
Syndee <cynthiaineh02@gmail.com>
2021-03-08 21:01:00 +0100

    updates

---
+++ src/components/RegisterPage.vue
@@ -0,0 +1,25 @@
+<template>
+<div class="main_div">
+<h1> amazing</h1>
+</div>
+</template>
+
+<script>
+export default {
+name: "Register"
+}
+</script>
+
+<style scoped>
+.main_div {
+  background: url('../assets/backgroundImg.svg') no-repeat center center fixed;
+  background-size: cover;
+  height: 100vh;
+  //background-image: url("@/assets/backgroundImg.svg");
+}
+
+//div[data-app='true'] {
+//  background: url('/assets/backgroundImg.svg') no-repeat center center fixed !important;
+//  background-size: cover;
+//}
+</style>
